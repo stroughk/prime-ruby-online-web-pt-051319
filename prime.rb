c8a57c8668511f73abf68066d3547271b798f6df
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 23:16:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 
 def prime?(num)
-  if num % num == 0 
-   true 
-   if num % num != 0 
-   false
-   end 
-end 
+if num < 0 || num == 0 || num == 1
+return false
+else
+(2..num-1).to_a.all? do |possible_factor|
+  num % possible_factor != 0
+end
+end
+end
 
-
-prime?(13)
+puts prime?(13)
