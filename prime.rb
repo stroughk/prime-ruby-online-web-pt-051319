c9906dd4245e2c3b4c812e968bb55a31d18f5229
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 23:18:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num < 0 || num == 0 || num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |num2|
+    num % num2 != 0
+    end
+  end
+end
+
+puts prime?(13)
