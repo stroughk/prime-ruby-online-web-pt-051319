4972e13d357f0e25611543898d2e48d90d5bc8f8
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 23:18:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
-
 def prime?(num)
-if num < 0 || num == 0 || num == 1
-return false
-else
-(2..num-1).to_a.all? do |num2|
-  num % num2 != 0
-end
-end
+  if num < 0 || num == 0 || num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |num2|
+    num % num2 != 0
+    end
+  end
 end
 
 puts prime?(13)
