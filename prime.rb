9edbf97faf8c19eb5abb7d744b46e20f9d9040fc
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-25 23:17:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(num)
 if num < 0 || num == 0 || num == 1
 return false
 else
-(2..num-1).to_a.all? do |possible_factor|
-  num % possible_factor != 0
+(2..num-1).to_a.all? do |num2|
+  num % num2 != 0
 end
 end
 end
